d4f8709d50e2c655c567c079e000dceab556d536
박병욱 <quddnr2688@gmail.com>
2024-09-30 12:14:34 +0900

    MUS-10 좋아요 도메인 실패 테스트 추가.

--- common/src/test/java/com/music/common/likes/domain/LikesTest.java
+++ common/src/test/java/com/music/common/likes/domain/LikesTest.java
@@ -6,11 +6,12 @@ import fixtures.BoardFixture;
 import fixtures.UserFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class LikesTest {
-
     private User user;
     private Board board;
 
@@ -31,5 +32,31 @@ class LikesTest {
         assertThat(likes).isNotNull();
         assertThat(likes.getUser()).isEqualTo(user);
         assertThat(likes.getBoard()).isEqualTo(board);
+    }
+
+    @Test
+    void 좋아요_생성_실패_유저_null() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Likes.create(null, board));
+    }
+
+    @Test
+    void 좋아요_생성_실패_게시글_null() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Likes.create(user, null));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void 좋아요_생성_실패_유저_null_혹은_빈값(User invalidUser) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Likes.create(invalidUser, board));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void 좋아요_생성_실패_게시글_null_혹은_빈값(Board invalidBoard) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Likes.create(user, invalidBoard));
     }
 }
