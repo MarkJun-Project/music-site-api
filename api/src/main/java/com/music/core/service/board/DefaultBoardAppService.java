d36c704d06cc334ed8260311d658bd3a0586f820
박병욱 <quddnr2688@gmail.com>
2024-10-14 23:14:16 +0900

    MUS-33 게시글 앱 서비스 수정

--- api/src/main/java/com/music/core/service/board/DefaultBoardAppService.java
+++ api/src/main/java/com/music/core/service/board/DefaultBoardAppService.java
@@ -1,13 +1,12 @@
 package com.music.core.service.board;
 
-import com.music.common.attachment.domain.Attachment;
 import com.music.common.attachment.service.AttachmentService;
 import com.music.common.board.domain.Board;
 import com.music.common.board.service.BoardService;
 import com.music.common.code.MusicCategory;
 import com.music.integration.storege.FileStore;
-import com.music.integration.storege.UploadFile;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,8 +22,8 @@ public class DefaultBoardAppService implements BoardAppService{
 
     @Override
     public void create(Long userId, String title, String description, String songTitle, MusicCategory category, MultipartFile multipartFile) {
-        UploadFile uploadFile = fileStore.upload(multipartFile, Board.DIR_NAME);
-        Attachment attachment = attachmentService.create(uploadFile.getFileUrl(), uploadFile.getFileName(), uploadFile.getOriginFileName());
+        val uploadFile = fileStore.upload(multipartFile, Board.DIR_NAME);
+        val attachment = attachmentService.create(uploadFile.getFileUrl(), uploadFile.getFileName(), uploadFile.getOriginFileName());
         boardService.create(userId, attachment.getId(), title, description, songTitle, category);
     }
 }
